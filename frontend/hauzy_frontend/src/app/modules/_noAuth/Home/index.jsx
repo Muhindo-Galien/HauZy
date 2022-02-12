d77be5d85cefb8b1468e9e71e558bc0e39046a73
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-12 18:22:47 +0200

    LatestProperty and ClientsReview component added

--- frontend/hauzy_frontend/src/app/modules/_noAuth/Home/index.jsx
+++ frontend/hauzy_frontend/src/app/modules/_noAuth/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import ClientsReview from './_modules_/ClientsReview';
+import LatestProperty from './_modules_/LatestProperty';
 import PresentationContainer from './_modules_/PresentationContainer';
 
 const HomePage = () => {
     return (
         <div>
             <PresentationContainer />
+            <LatestProperty />
+            <ClientsReview/>
         </div>
     );
 };

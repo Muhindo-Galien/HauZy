c1e9ad7a1d06317e0fc3c1c38ff70ebb5a1b8bbf
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-14 15:50:34 +0200

    google font Mulish and Poppins added

--- frontend/hauzy_frontend/tailwind.config.js
+++ frontend/hauzy_frontend/tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
       fontFamily: {
         Mulish: ['Mulish', 'sans-serif'],
         Poppins: ['Poppins', 'sans-serif'],
-      }
+      },
+      screens: {
+        'xsm': {'min':'300px', 'max':'399px'},
+        'sm': {'min': '400px', 'max': '767px'},
+        'md': { 'min': '768px', 'max': '1023px' },
+        'lg': {'min': '1024px', 'max': '1279px'},
+        'xl': { 'min': '1280px', 'max': '1535px' },
+        '2xl': {'min': '1536px'},
+      },
   }, },
   plugins: [],
 }

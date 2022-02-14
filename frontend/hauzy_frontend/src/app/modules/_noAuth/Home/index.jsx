b1fdd3b2e4335fcb57d3405b1267da04e86a25bc
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-14 15:49:57 +0200

    Footer added

--- frontend/hauzy_frontend/src/app/modules/_noAuth/Home/index.jsx
+++ frontend/hauzy_frontend/src/app/modules/_noAuth/Home/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ClientsReview from './_modules_/ClientsReview';
-import LatestProperty from './_modules_/LatestProperty';
+import Footer from './_modules_/Footer';
+import LatestProperties from './_modules_/LatestProperty';
 import PresentationContainer from './_modules_/PresentationContainer';
 
 const HomePage = () => {
     return (
         <div>
             <PresentationContainer />
-            <LatestProperty />
-            <ClientsReview/>
+            <LatestProperties />
+            <ClientsReview />
+            <Footer/>
         </div>
     );
 };
